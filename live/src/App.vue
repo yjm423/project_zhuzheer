f33da89068d06bef69e794476b7b3b7e76ee22b9
yjm423 <1604813613@qq.com>
2020-01-15 14:37:28 +0800

    一级路由搭建，各自认领任务

--- live/src/App.vue
+++ live/src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    
+    <layout></layout>
   </div>
 </template>
 
+<script>
+import Layout from './components/layout'
+export default {
+   name: 'app',
+  components: {
+   
+    Layout
+  }
+}
+</script>
 <style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+
 </style>
---
+++ live/src/pages/loginregister/login.vue
@@ -0,0 +1,3 @@
+<template>
+    <h3>登录页</h3>
+</template>
