b9a82c8b184991a7d63aadc496f0189ce726d160
yjm423 <1604813613@qq.com>
2021-04-18 14:50:11 +0800

    第一次提交

--- live/src/pages/market/Soduko/drinking.vue
+++ live/src/pages/market/Soduko/drinking.vue
@@ -1,5 +1,59 @@
+
 <template>
     <div>
-         喝水
+         <div class="btn" @click="inc">+1</div>
+         <!-- <div class="btn" @click="add">+1</div> -->
+         <!-- <div class="btn" @click="INCREMENT">+1</div> -->
+         
+         <!-- 后非标准 直接从state里面取值 -->
+         <!-- 后标志 从getters里面的newState取值 -->
+         {{this.$store.state.count}}
+         {{this.$store.getters.newState}}
+         <p>{{n}}</p>
     </div>
 </template>
+<script>
+import {mapState,mapActions,mapMutations,mapGetters} from 'vuex'
+export default {
+    data(){
+        return{
+            count:1
+        }
+    },
+    methods:{
+       inc(){
+           this.$store.dispatch('add',5)
+
+        /**
+         * 前非标准
+         */
+    //        this.$store.commit('INCREMENT',5)
+    //        console.log(this.$store)
+       }
+
+    /**
+     * vuex辅助工具  方法统一获取到 但是是以数组的形式
+     */
+    // ...mapActions(['add']),
+    // ...mapMutations(['INCREMENT'])
+
+    },
+
+    computed:{
+        ...mapState({
+            n:state=>state.count
+        })
+    }
+
+    
+}
+</script>
+<style lang="stylus" scoped>
+    .btn{
+        height 0.3rem
+        width 0.6rem
+        border 1px solid #ccc
+        background-color #ccc
+        margin 0.2rem auto
+    }
+</style>
--- live/src/store/index.js
+++ live/src/store/index.js
@@ -1,15 +1,36 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import * as type from './type'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-  },
-  mutations: {
+    count:0
   },
   actions: {
+    add({commit},payload){
+      var  actionType={
+        type:type.INCREMENT,
+        number:payload  //组件传过来的值
+      }
+      commit(actionType)  //将actions传给mutations
+    }
   },
-  modules: {
+
+  //接收传过来的actions 对状态进行更改 
+  mutations: {
+   [type.INCREMENT](state,action){
+      state.count++
+
+      //如果前面是dispatch非标志 那么action就是传过来的参数
+      console.log(action)
+   }
+  },
+ 
+  
+  getters:{
+    newState(state){
+      return state.count
+    }
   }
 })
